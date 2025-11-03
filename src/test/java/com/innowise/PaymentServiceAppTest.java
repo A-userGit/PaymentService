0872ea9ba1d6cb3d3d816d9d77e81bf7dfb1a2f3
asemchenok <asemchenok@gmail.com>
2025-11-03 14:37:07 +0300

    initial commit

---
+++ src/test/java/com/innowise/PaymentServiceAppTest.java
@@ -0,0 +1,38 @@
+package com.innowise;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class PaymentServiceAppTest
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public PaymentServiceAppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( PaymentServiceAppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
